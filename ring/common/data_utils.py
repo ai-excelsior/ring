6b2b190fd20d8d21d9bcc008741870120f379ac4
xu-yizhou-faust <yizhou.xu@unianalysis.com>
2022-04-12 13:42:57 +0800

    enable read_parquet

--- ring/common/data_utils.py
+++ ring/common/data_utils.py
@@ -6,7 +6,17 @@ from .utils import remove_prefix
 def read_csv(url: str, **config) -> pd.DataFrame:
     if url.startswith("oss://"):
         bucket, key = get_bucket_from_oss_url(url)
-        return pd.read_csv(bucket.get_object(key), thousands=",")
+        if bucket.get_object(key)[-4:] == ".csv":
+            return pd.read_csv(bucket.get_object(key), thousands=",")
+        elif bucket.get_object(key)[-5:] == ".parq":
+            return pd.read_parquet(bucket.get_object(key))
+        else:
+            raise TypeError("Only .csv or .parq can be accessed")
     else:
         filepath = remove_prefix(url, "file://")
-        return pd.read_csv(filepath, thousands=",", **config)
+        if filepath[-4:] == ".csv":
+            return pd.read_csv(filepath, thousands=",", **config)
+        elif filepath[-5:] == ".parq":
+            return pd.read_parquet(filepath)
+        else:
+            raise TypeError("Only .csv or .parq can be accessed")
