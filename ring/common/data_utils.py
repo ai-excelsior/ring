0711c03d2f7b6a25f5a5f2e0f56c7e37979b79a7
Li Yu <lijingyu68@gmail.com>
2022-04-18 11:19:46 +0800

    [TECH] refactor & fix bug.

--- ring/common/data_utils.py
+++ ring/common/data_utils.py
@@ -3,20 +3,28 @@ from .oss_utils import get_bucket_from_oss_url
 from .utils import remove_prefix
 
 
+def is_parquet(s: str):
+    return s.endswith(".parq") or s.endswith(".parquet")
+
+
+def is_csv(s: str):
+    return s.endswith(".csv")
+
+
 def read_csv(url: str, **config) -> pd.DataFrame:
     if url.startswith("oss://"):
         bucket, key = get_bucket_from_oss_url(url)
-        if bucket.get_object(key)[-4:] == ".csv":
-            return pd.read_csv(bucket.get_object(key), thousands=",")
-        elif bucket.get_object(key)[-5:] == ".parq":
-            return pd.read_parquet(bucket.get_object(key))
+        if is_csv(key):
+            return pd.read_csv(bucket.get_object(key), thousands=",", **config)
+        elif is_parquet(key):
+            return pd.read_parquet(bucket.get_object(key), **config)
         else:
-            raise TypeError("Only .csv or .parq can be accessed")
+            raise TypeError("Only .csv .parq or .parquet can be accessed")
     else:
         filepath = remove_prefix(url, "file://")
-        if filepath[-4:] == ".csv":
+        if is_csv(filepath):
             return pd.read_csv(filepath, thousands=",", **config)
-        elif filepath[-5:] == ".parq":
-            return pd.read_parquet(filepath)
+        elif is_parquet(filepath):
+            return pd.read_parquet(filepath, **config)
         else:
-            raise TypeError("Only .csv or .parq can be accessed")
+            raise TypeError("Only .csv .parq or .parquet can be accessed")
