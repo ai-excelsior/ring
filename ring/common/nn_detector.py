9f129c893e66d8e07f3e95f5320a9c23365e3cd7
xu-yizhou-faust <yizhou.xu@unianalysis.com>
2022-06-15 09:50:14 +0800

    epoch_length

--- ring/common/nn_detector.py
+++ ring/common/nn_detector.py
@@ -245,7 +245,10 @@ class Detector:
 
         @trainer.on(Events.EPOCH_COMPLETED)
         def run_validation(trainer):
-            evaluator.run(val_dataloader, 2 * len(val_dataloader) // self._trainer_cfg.get("max_epochs", 100))
+            evaluator.run(
+                val_dataloader,
+                epoch_length=max(len(val_dataloader) // 10 // self._trainer_cfg.get("max_epochs", 100), 100),
+            )
             metrics = evaluator.state.metrics
             print(
                 f"Training Results - Epoch: {trainer.state.epoch}, {self._loss_cfg} Loss: {trainer.state.output:.2f}"
@@ -293,7 +296,7 @@ class Detector:
         def evalutate_parameter():
             gaussian_parameters.run(
                 gaussian_loader,
-                epoch_length=2 * len(gaussian_loader) // self._trainer_cfg.get("max_epochs", 100),
+                epoch_length=max(len(gaussian_loader) // 10 // self._trainer_cfg.get("max_epochs", 100), 100),
             )
             parameters = model.calculate_params(**gaussian_parameters.state.output)
             self._model_states.update(**parameters)
@@ -342,7 +345,9 @@ class Detector:
             trainer.run(
                 train_dataloader,
                 max_epochs=self._trainer_cfg.get("max_epochs", 100),
-                epoch_length=2 * len(train_dataloader) // self._trainer_cfg.get("max_epochs", 100),
+                epoch_length=max(
+                    len(train_dataloader) // 10 // self._trainer_cfg.get("max_epochs", 100), 100
+                ),
             )
         self.save()
 
