b13e9c9b7503c60152d26964f68b4caaa69438e3
xu-yizhou-faust <yizhou.xu@unianalysis.com>
2022-06-28 10:32:39 +0800

    remove upload in train

--- ring/anomal/dagmm/main.py
+++ ring/anomal/dagmm/main.py
@@ -58,10 +58,10 @@ def train(data_config: DataConfig, data_train: pd.DataFrame, data_val: pd.DataFr
 
     if load_state is None:
         print(f"Model saved in local file path: {predictor.save_dir}")
-        if kwargs["save_state"].startswith("oss://"):
-            predictor.upload(kwargs["save_state"])
-    else:
-        predictor.upload(kwargs["save_state"])
+    #     if kwargs["save_state"].startswith("oss://"):
+    #         predictor.upload(kwargs["save_state"])
+    # else:
+    #     predictor.upload(kwargs["save_state"])
 
 
 def validate(load_state: str, data_val: pd.DataFrame):
